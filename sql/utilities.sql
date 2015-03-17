b62d7205795c6e8c32f051eee04419b50b63f5cf
Jason Petersen <jason@citusdata.com>
2015-03-16 21:51:34 -0600

    Add test for PREPARE command
    Not currently supported by pg_shard.

--- sql/utilities.sql
+++ sql/utilities.sql
@@ -15,3 +15,6 @@ DECLARE all_sharded_rows CURSOR FOR SELECT * FROM sharded_table;
 
 -- EXPLAIN support isn't implemented
 EXPLAIN SELECT * FROM sharded_table;
+
+-- PREPARE support isn't implemented
+PREPARE sharded_query (bigint) AS SELECT * FROM sharded_table WHERE id = $1;
