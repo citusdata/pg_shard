033806ce72c4672e262984f12db96758c197d46c
Jason Petersen <jason@citusdata.com>
2015-03-16 21:18:49 -0600

    Clean up comments in shard pruning test

--- sql/prune_shard_list.sql
+++ sql/prune_shard_list.sql
@@ -31,6 +31,7 @@ CREATE FUNCTION debug_equality_expression(regclass)
 -- test shard pruning functionality
 -- ===================================================================
 
+-- create distributed table metadata to observe shard pruning
 CREATE TABLE pruning ( species text, last_pruned date, plant_id integer );
 
 INSERT INTO pgs_distribution_metadata.partition (relation_id, partition_method, key)
@@ -45,16 +46,23 @@ VALUES
 	(12, 'pruning'::regclass, 't', '-2', '1073741820'),
 	(13, 'pruning'::regclass, 't', '1073741821', '2147483647');
 
+-- with no values, expect all shards
 SELECT prune_using_no_values('pruning');
 
+-- with a single value, expect a single shard
 SELECT prune_using_single_value('pruning', 'tomato');
 
+-- the above is true even if that value is null
 SELECT prune_using_single_value('pruning', NULL);
 
+-- build an OR clause and expect more than one sahrd
 SELECT prune_using_either_value('pruning', 'tomato', 'petunia');
 
+-- an AND clause with incompatible values returns no shards
 SELECT prune_using_both_values('pruning', 'tomato', 'petunia');
 
+-- but if both values are on the same shard, should get back that shard
 SELECT prune_using_both_values('pruning', 'tomato', 'rose');
 
+-- unit test of the equality expression generation code
 SELECT debug_equality_expression('pruning');
