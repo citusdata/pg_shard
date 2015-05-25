c3515be010bb4abccfef9c8c19ae44be13570ab3
Jason Petersen <jason@citusdata.com>
2015-05-25 10:39:40 -0700

    Clean up fake_fdw file
    Added comments, fixed includes, etc.

--- test/fake_fdw.c
+++ test/fake_fdw.c
@@ -1,14 +1,40 @@
-#include "postgres.h"
+/*-------------------------------------------------------------------------
+ *
+ * test/fake_fdw.c
+ *
+ * This file contains a barebones FDW implementation, suitable for use in
+ * test code. Inspired by Andrew Dunstan's blackhole_fdw.
+ *
+ * Copyright (c) 2014-2015, Citus Data, Inc.
+ *
+ *-------------------------------------------------------------------------
+ */
 
-#include "access/reloptions.h"
+#include "postgres.h"
+#include "c.h"
+#include "fmgr.h"
+#include "postgres_ext.h"
+
+#include "test/test_helper_functions.h" /* IWYU pragma: keep */
+
+#include <stddef.h>
+
+#include "executor/tuptable.h"
 #include "foreign/fdwapi.h"
-#include "foreign/foreign.h"
+#include "nodes/execnodes.h"
+#include "nodes/nodes.h"
+#include "nodes/pg_list.h"
+#include "nodes/plannodes.h"
+#include "nodes/relation.h"
 #include "optimizer/pathnode.h"
 #include "optimizer/planmain.h"
 #include "optimizer/restrictinfo.h"
+#include "utils/palloc.h"
 
-PG_FUNCTION_INFO_V1(fake_fdw_handler);
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
 
+/* local function forward declarations */
 static void FakeGetForeignRelSize(PlannerInfo *root, RelOptInfo *baserel,
 								  Oid foreigntableid);
 static void FakeGetForeignPaths(PlannerInfo *root, RelOptInfo *baserel,
@@ -22,12 +48,18 @@ static void FakeReScanForeignScan(ForeignScanState *node);
 static void FakeEndForeignScan(ForeignScanState *node);
 
 
+/* declarations for dynamic loading */
+PG_FUNCTION_INFO_V1(fake_fdw_handler);
+
+
+/*
+ * fake_fdw_handler populates an FdwRoutine with pointers to the functions
+ * implemented within this file.
+ */
 Datum
-fake_fdw_handler(PG_FUNCTION_ARGS __attribute__((unused)))
+fake_fdw_handler(PG_FUNCTION_ARGS)
 {
 	FdwRoutine *fdwroutine = makeNode(FdwRoutine);
-
-	/* Required by notations: S=SELECT I=INSERT U=UPDATE D=DELETE */
 
 	fdwroutine->GetForeignRelSize = FakeGetForeignRelSize;
 	fdwroutine->GetForeignPaths = FakeGetForeignPaths;
@@ -41,40 +73,38 @@ fake_fdw_handler(PG_FUNCTION_ARGS __attribute__((unused)))
 }
 
 
+/*
+ * FakeGetForeignRelSize populates baserel with a fake relation size.
+ */
 static void
-FakeGetForeignRelSize(PlannerInfo *root __attribute__((unused)), RelOptInfo *baserel,
-					  Oid foreigntableid __attribute__((unused)))
+FakeGetForeignRelSize(PlannerInfo *root, RelOptInfo *baserel, Oid foreigntableid)
 {
 	baserel->rows = 0;
 	baserel->fdw_private = (void *) palloc0(1);
 }
 
 
+/*
+ * FakeGetForeignPaths adds a single fake foreign path to baserel.
+ */
 static void
-FakeGetForeignPaths(PlannerInfo *root, RelOptInfo *baserel,
-					Oid foreigntableid __attribute__((unused)))
+FakeGetForeignPaths(PlannerInfo *root, RelOptInfo *baserel, Oid foreigntableid)
 {
 	Cost startup_cost = 0;
 	Cost total_cost = startup_cost + baserel->rows;
 
-	add_path(baserel, (Path *)
-			 create_foreignscan_path(root, baserel,
-									 baserel->rows,
-									 startup_cost,
-									 total_cost,
-									 NIL,
-									 NULL,
-									 NIL));
+	add_path(baserel, (Path *) create_foreignscan_path(root, baserel, baserel->rows,
+													   startup_cost, total_cost, NIL,
+													   NULL, NIL));
 }
 
 
+/*
+ * FakeGetForeignPlan builds a fake foreign plan.
+ */
 static ForeignScan *
-FakeGetForeignPlan(PlannerInfo *root __attribute__((unused)),
-				   RelOptInfo *baserel __attribute__((unused)),
-				   Oid foreigntableid __attribute__((unused)),
-				   ForeignPath *best_path __attribute__((unused)),
-				   List *tlist __attribute__((unused)),
-				   List *scan_clauses __attribute__((unused)))
+FakeGetForeignPlan(PlannerInfo *root, RelOptInfo *baserel, Oid foreigntableid,
+				   ForeignPath *best_path, List *tlist, List *scan_clauses)
 {
 	Index scan_relid = baserel->relid;
 	scan_clauses = extract_actual_clauses(scan_clauses, false);
@@ -83,11 +113,16 @@ FakeGetForeignPlan(PlannerInfo *root __attribute__((unused)),
 }
 
 
+/*
+ * FakeBeginForeignScan begins the fake plan (i.e. does nothing).
+ */
 static void
-FakeBeginForeignScan(ForeignScanState *node __attribute__((unused)),
-					 int eflags __attribute__((unused))) { }
+FakeBeginForeignScan(ForeignScanState *node, int eflags) { }
 
 
+/*
+ * FakeIterateForeignScan continues the fake plan (i.e. does nothing).
+ */
 static TupleTableSlot *
 FakeIterateForeignScan(ForeignScanState *node)
 {
@@ -98,9 +133,18 @@ FakeIterateForeignScan(ForeignScanState *node)
 }
 
 
+/*
+ * FakeReScanForeignScan restarts the fake plan (i.e. does nothing).
+ */
 static void
-FakeReScanForeignScan(ForeignScanState *node __attribute__((unused))) { }
+FakeReScanForeignScan(ForeignScanState *node) { }
 
 
+/*
+ * FakeEndForeignScan ends the fake plan (i.e. does nothing).
+ */
 static void
-FakeEndForeignScan(ForeignScanState *node __attribute__((unused))) { }
+FakeEndForeignScan(ForeignScanState *node) { }
+
+
+#pragma GCC diagnostic pop
