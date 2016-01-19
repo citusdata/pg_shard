ed9ec877722379a2cae5e59a12cff2baa02d99fb
knizhnik <knizhnik@garret.ru>
2016-01-19 23:12:38 +0300

    uncrusify pg_tmgr.[hc]

--- include/pg_tmgr.h
+++ include/pg_tmgr.h
@@ -12,17 +12,18 @@
 #ifndef PG_TMGR_H
 #define PG_TMGR_H
 
-typedef struct { 
-	bool (*Begin)(PGconn* conn);
-	bool (*Prepare)(PGconn* conn);
-	bool (*CommitPrepared)(PGconn* conn);
-	bool (*RollbackPrepared)(PGconn* conn);
-	bool (*Rollback)(PGconn* conn);
+typedef struct
+{
+	bool (*Begin)(PGconn *conn);
+	bool (*Prepare)(PGconn *conn);
+	bool (*CommitPrepared)(PGconn *conn);
+	bool (*RollbackPrepared)(PGconn *conn);
+	bool (*Rollback)(PGconn *conn);
 } PgShardTransactionManager;
 
 extern int PgShardCurrTransManager;
 extern PgShardTransactionManager const PgShardTransManagerImpl[];
 
-extern bool PgShardExecute(PGconn* conn, ExecStatusType expectedResult, char const* sql);
+extern bool PgShardExecute(PGconn *conn, ExecStatusType expectedResult, char const *sql);
 
 #endif
--- src/pg_tmgr.c
+++ src/pg_tmgr.c
@@ -9,100 +9,118 @@
 
 int PgShardCurrTransManager;
 
-static bool PgShardBeginStub(PGconn* conn);
-static bool PgShardPrepareStub(PGconn* conn);
-static bool PgShardCommitPreparedStub(PGconn* conn);
-static bool PgShardRollbackPreparedStub(PGconn* conn);
-static bool PgShardRollbackStub(PGconn* conn);
+static bool PgShardBeginStub(PGconn *conn);
+static bool PgShardPrepareStub(PGconn *conn);
+static bool PgShardCommitPreparedStub(PGconn *conn);
+static bool PgShardRollbackPreparedStub(PGconn *conn);
+static bool PgShardRollbackStub(PGconn *conn);
 
-static bool PgShardBegin2PC(PGconn* conn);
-static bool PgShardPrepare2PC(PGconn* conn);
-static bool PgShardCommitPrepared2PC(PGconn* conn);
-static bool PgShardRollbackPrepared2PC(PGconn* conn);
-static bool PgShardRollback2PC(PGconn* conn);
+static bool PgShardBegin2PC(PGconn *conn);
+static bool PgShardPrepare2PC(PGconn *conn);
+static bool PgShardCommitPrepared2PC(PGconn *conn);
+static bool PgShardRollbackPrepared2PC(PGconn *conn);
+static bool PgShardRollback2PC(PGconn *conn);
 
-PgShardTransactionManager const PgShardTransManagerImpl[] = 
+PgShardTransactionManager const PgShardTransManagerImpl[] =
 {
-	{ PgShardBeginStub, PgShardPrepareStub, PgShardCommitPreparedStub, PgShardRollbackPreparedStub, PgShardRollbackStub },
-	{ PgShardBegin2PC, PgShardPrepare2PC, PgShardCommitPrepared2PC, PgShardRollbackPrepared2PC, PgShardRollback2PC }
+	{ PgShardBeginStub, PgShardPrepareStub, PgShardCommitPreparedStub,
+	  PgShardRollbackPreparedStub, PgShardRollbackStub },
+	{ PgShardBegin2PC, PgShardPrepare2PC, PgShardCommitPrepared2PC,
+	  PgShardRollbackPrepared2PC, PgShardRollback2PC }
 };
 
-/* 
+/*
  * Transaction manager stub
  */
-static bool PgShardBeginStub(PGconn* conn) 
+static bool
+PgShardBeginStub(PGconn *conn)
 {
 	return true;
 }
 
-static bool PgShardPrepareStub(PGconn* conn)
+
+static bool
+PgShardPrepareStub(PGconn *conn)
 {
 	return true;
 }
 
-static bool PgShardCommitPreparedStub(PGconn* conn)
+
+static bool
+PgShardCommitPreparedStub(PGconn *conn)
 {
 	return true;
 }
 
-static bool PgShardRollbackPreparedStub(PGconn* conn)
+
+static bool
+PgShardRollbackPreparedStub(PGconn *conn)
 {
 	return true;
 }
 
-static bool PgShardRollbackStub(PGconn* conn)
+
+static bool
+PgShardRollbackStub(PGconn *conn)
 {
 	return true;
 }
 
-/* 
- * Two-phase commit 
- */ 
+
+/*
+ * Two-phase commit
+ */
 static int GlobalTransactionId;
 
-static char* 
-PgShard2pcCommand(char const* cmd)
+static char *
+PgShard2pcCommand(char const *cmd)
 {
-    return psprintf("%s 'pgshard_%d_%d'", cmd, MyProcPid, GlobalTransactionId);
+	return psprintf("%s 'pgshard_%d_%d'", cmd, MyProcPid, GlobalTransactionId);
 }
 
 
-static bool 
-PgShardBegin2PC(PGconn* conn)
+static bool
+PgShardBegin2PC(PGconn *conn)
 {
 	return PgShardExecute(conn, PGRES_COMMAND_OK, "BEGIN TRANSACTION");
 }
 
-static bool 
-PgShardPrepare2PC(PGconn* conn)
+
+static bool
+PgShardPrepare2PC(PGconn *conn)
 {
 	GlobalTransactionId += 1;
-	return PgShardExecute(conn, PGRES_COMMAND_OK, PgShard2pcCommand("PREPARE TRANSACTION"));
-}
-							
-static bool 
-PgShardCommitPrepared2PC(PGconn* conn)
-{
-	return PgShardExecute(conn, PGRES_COMMAND_OK, PgShard2pcCommand("COMMIT PREPARED")); 
+	return PgShardExecute(conn, PGRES_COMMAND_OK, PgShard2pcCommand(
+							  "PREPARE TRANSACTION"));
 }
 
-static bool 
-PgShardRollbackPrepared2PC(PGconn* conn)
+
+static bool
+PgShardCommitPrepared2PC(PGconn *conn)
 {
-	return PgShardExecute(conn, PGRES_COMMAND_OK, PgShard2pcCommand("ROLLBACK PREPARED")); 
+	return PgShardExecute(conn, PGRES_COMMAND_OK, PgShard2pcCommand("COMMIT PREPARED"));
 }
 
-static bool 
-PgShardRollback2PC(PGconn* conn)
+
+static bool
+PgShardRollbackPrepared2PC(PGconn *conn)
+{
+	return PgShardExecute(conn, PGRES_COMMAND_OK, PgShard2pcCommand("ROLLBACK PREPARED"));
+}
+
+
+static bool
+PgShardRollback2PC(PGconn *conn)
 {
 	return PgShardExecute(conn, PGRES_COMMAND_OK, "ROLLBACK");
 }
 
+
 /*
  * Execute statement with specified parameters and check its result
  */
-bool 
-PgShardExecute(PGconn* conn, ExecStatusType expectedResult, char const* sql)
+bool
+PgShardExecute(PGconn *conn, ExecStatusType expectedResult, char const *sql)
 {
 	bool ret = true;
 	PGresult *result = PQexec(conn, sql);
@@ -114,8 +132,3 @@ PgShardExecute(PGconn* conn, ExecStatusType expectedResult, char const* sql)
 	PQclear(result);
 	return ret;
 }
-
-
-
-
-
